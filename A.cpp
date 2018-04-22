fae097fb024feb73d2ac07c87c15115685f6392c
Ismail Hossain <shaikat.ru.39@gmail.com>
2018-04-22 11:06:13 -0700

    Codeforces contest 475(964) upsolve

---
+++ A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sc(a) scanf("%d",&a)
+int main()
+{
+    ll n;
+
+    cin>>n;
+    cout<<(n/2)+1<<endl;
+
+    return 0;
+}
