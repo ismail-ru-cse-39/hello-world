a3e8a6c2a700ff1c5e85d608eea2834d791fc412
Ismail Hossain <shaikat.ru.39@gmail.com>
2018-10-04 21:11:25 -0700

    Greedy

---
+++ 1060B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n,ans =0;
+
+    cin>>n;
+
+    while(n > 9){
+        n -= 9;
+        ans += 9;
+        ans += n%10;
+        n /= 10;
+    }
+
+    cout<<ans+n<<endl;
+
+    return 0;
+}
